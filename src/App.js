83032e905521435edca4c11ccdb0da417091fde6
AgrisStrautnieks <62353995+AgrisStrautnieks@users.noreply.github.com>
2022-02-23 22:38:36 +0200

    made needed changes for nav links

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import Layout from "./components/Layout";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import PageOne from "./pages/PageOne";
-import PageTwo from "./pages/PageTwo";
-import PageThree from "./pages/PageThree";
+import AboutUs from "./pages/AboutUs";
+import Products from "./pages/Products";
+import Contacts from "./pages/Contacts";
 import PageCTA from "./pages/PageCTA";
 
 function App() {
-    return (
-        <Layout>
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/page-one">
-                    <PageOne />
-                </Route>
-                <Route path="/page-two">
-                    <PageTwo />
-                </Route>
-                <Route path="/page-three">
-                    <PageThree />
-                </Route>
-                <Route path="/page-cta">
-                    <PageCTA />
-                </Route>
-            </Switch>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/AboutUs">
+          <AboutUs />
+        </Route>
+        <Route path="/Products">
+          <Products />
+        </Route>
+        <Route path="/Contacts">
+          <Contacts />
+        </Route>
+        <Route path="/page-cta">
+          <PageCTA />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
